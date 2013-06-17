1841e208d2e5265ac7e4da9dcfa6abdb03244897
Brett <brett.alcorn@insidemind.com.au>
2013-06-17 10:56:43 +1000

    added more lines to scripts

--- build.bat
+++ build.bat
@@ -2,7 +2,16 @@
 
 echo This is the build script
 
-ping 1.1.1.1 -n 1 -w 3000
+echo build 1 of 5
+ping 1.1.1.1 -n 1 -w 3000 >nul
+echo build 2 of 5
+ping 1.1.1.1 -n 1 -w 3000 >nul
+echo build 3 of 5
+ping 1.1.1.1 -n 1 -w 3000 >nul
+echo build 4 of 5
+ping 1.1.1.1 -n 1 -w 3000 >nul
+echo build 5 of 5
+ping 1.1.1.1 -n 1 -w 3000 >nul
 
 echo Success!!
 exit /b 0
--- test.bat
+++ test.bat
@@ -2,7 +2,13 @@
 
 echo This is the test script
 
-ping 1.1.1.1 -n 1 -w 3000
+echo test 1 of 3
+ping 1.1.1.1 -n 1 -w 3000 >nul
+echo test 2 of 3
+ping 1.1.1.1 -n 1 -w 3000 >nul
+echo test 3 of 3
+ping 1.1.1.1 -n 1 -w 3000 >nul
 
 echo Success!!
 exit /b 0
+
