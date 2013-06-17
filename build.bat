cc780b10e662a5f7ae4ef150ea2a55d8e0d15f3c
Brett <brett.alcorn@insidemind.com.au>
2013-06-17 11:02:42 +1000

    added more lines to scripts

--- build.bat
+++ build.bat
@@ -3,15 +3,15 @@
 echo This is the build script
 
 echo build 1 of 5
-ping 1.1.1.1 -n 1 -w 3000 >nul
+ping 1.1.1.1 -n 1 -w 5000 >nul
 echo build 2 of 5
-ping 1.1.1.1 -n 1 -w 3000 >nul
+ping 1.1.1.1 -n 1 -w 5000 >nul
 echo build 3 of 5
-ping 1.1.1.1 -n 1 -w 3000 >nul
+ping 1.1.1.1 -n 1 -w 5000 >nul
 echo build 4 of 5
-ping 1.1.1.1 -n 1 -w 3000 >nul
+ping 1.1.1.1 -n 1 -w 5000 >nul
 echo build 5 of 5
-ping 1.1.1.1 -n 1 -w 3000 >nul
+ping 1.1.1.1 -n 1 -w 5000 >nul
 
 echo Success!!
 exit /b 0
--- test.bat
+++ test.bat
@@ -2,12 +2,26 @@
 
 echo This is the test script
 
-echo test 1 of 3
-ping 1.1.1.1 -n 1 -w 3000 >nul
-echo test 2 of 3
-ping 1.1.1.1 -n 1 -w 3000 >nul
-echo test 3 of 3
-ping 1.1.1.1 -n 1 -w 3000 >nul
+echo test 1 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 2 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 3 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 4 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 5 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 6 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 7 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 8 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 9 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
+echo test 10 of 10
+ping 1.1.1.1 -n 1 -w 5000 >nul
 
 echo Success!!
 exit /b 0
